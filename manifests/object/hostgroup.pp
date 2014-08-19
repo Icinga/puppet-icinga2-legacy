561fa270cda4414915152ec20915c8b6b45a7d40
melanie <melanie.gault@ville-noumea.nc>
2014-08-20 10:21:06 +1100

    modify default path to reflect server.pp conf + lint

--- manifests/object/hostgroup.pp
+++ manifests/object/hostgroup.pp
@@ -11,25 +11,23 @@
 
 define icinga2::object::hostgroup (
   $object_hostgroup_name = $name,
-  $display_name = $name,
-  $template_to_import = undef,
-  $groups = [],
-  $target_dir = '/etc/icinga2/conf.d',
-  $target_file_name = "${name}.conf",
-  $target_file_owner = 'root',
-  $target_file_group = 'root',
-  $target_file_mode = '644',
-  $assign_where = undef,
-  $ignore_where = undef
-) {
-
-  file {"${target_dir}/${target_file_name}":
-    ensure => file,
+  $display_name          = $name,
+  $template_to_import    = undef,
+  $groups                = [],
+  $target_dir            = '/etc/icinga2/objects/hostgroups',
+  $target_file_name      = "${name}.conf",
+  $target_file_owner     = 'root',
+  $target_file_group     = 'root',
+  $target_file_mode      = '644',
+  $assign_where          = undef,
+  $ignore_where          = undef) {
+  file { "${target_dir}/${target_file_name}":
+    ensure  => file,
     owner   => $target_file_owner,
     group   => $target_file_group,
     mode    => $target_file_mode,
     content => template('icinga2/object_hostgroup.conf.erb'),
-    notify => Service['icinga2'],
+    notify  => Service['icinga2'],
   }
 
 }
