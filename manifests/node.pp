bcd162466aed4a192e0d50a22970e806f87d7e4a
Nick Chappell <nick@intronic.org>
2015-02-01 14:48:59 -0800

    Added an icinga2::node manifest with an empty icinga2::node class.

---
+++ manifests/node.pp
@@ -0,0 +1,15 @@
+# == Class: icinga2::node
+#
+# This module installs Icinga 2 and does some basic, general configuration that's applicable
+# to all of the different types of roles Icinga 2 can be used in (server, agent, satellite, etc.)
+#
+# === Parameters
+#
+# Coming soon...
+#
+# === Examples
+#
+# Coming soon...
+#
+
+class icinga2::node ( ) inherits icinga2::params { }
