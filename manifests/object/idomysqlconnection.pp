a70b9448cc6c95c26815b8a94a8069ab97d4cc17
Markus Frosch <markus@lazyfrosch.de>
2015-03-18 19:11:42 +0100

    Implement feature ido_mysql
    refs #8786
    refs #8789

--- manifests/object/idomysqlconnection.pp
+++ manifests/object/idomysqlconnection.pp
@@ -1,18 +1,11 @@
 # == Defined type: icinga2::object::idomysqlconnection
 #
 # This is a defined type for Icinga 2 IDO MySQL connection objects.
-# See the following Icinga 2 doc page for more info:
-# http://docs.icinga.org/icinga2/latest/doc/module/icinga2/chapter/configuring-icinga2#objecttype-idomysqlconnection
 #
-# === Parameters
-#
-# See the inline comments.
-#
-
 define icinga2::object::idomysqlconnection (
   $object_name          = $name,
   $host                 = 'localhost',
-  $port                 = 3306,
+  $port                 = undef,
   $user                 = 'icinga',
   $password             = 'icinga',
   $database             = 'icinga',
@@ -36,17 +29,15 @@ define icinga2::object::idomysqlconnection (
     servicechecks_age              => 0,
     systemcommands_age             => 0,
   },
-  $categories           = [],
-  $target_dir           = '/etc/icinga2/conf.d',
-  $target_file_ensure   = file,
-  $target_file_name     = "${name}.conf",
-  $target_file_owner    = 'root',
-  $target_file_group    = 'root',
-  $target_file_mode     = '0644',
-  $refresh_icinga2_service = true
+  $categories      = [],
+  $target_file     = "/etc/icinga2/conf.d/${name}.conf",
+  $refresh_service = $::icinga2::manage_service,
 ) {
 
-  #Do some validation of the class' parameters:
+  if ! defined(Class['icinga2']) {
+    fail('You must include the icinga2 base class before using any icinga2 defined resources')
+  }
+
   validate_string($object_name)
   validate_string($host)
   validate_string($user)
@@ -56,38 +47,21 @@ define icinga2::object::idomysqlconnection (
   validate_string($instance_name)
   validate_hash($cleanup)
   validate_array($categories)
-  validate_string($target_dir)
-  validate_string($target_file_name)
-  validate_string($target_file_owner)
-  validate_string($target_file_group)
-  validate_string($target_file_mode)
-  validate_bool($refresh_icinga2_service)
+  validate_absolute_path($target_file)
+  validate_bool($refresh_service)
 
-  #If the refresh_icinga2_service parameter is set to true...
-  if $refresh_icinga2_service == true {
-
-    file { "${target_dir}/${target_file_name}":
-      ensure  => $target_file_ensure,
-      owner   => $target_file_owner,
-      group   => $target_file_group,
-      mode    => $target_file_mode,
-      content => template('icinga2/object_idomysqlconnection.conf.erb'),
-      #...notify the Icinga 2 daemon so it can restart and pick up changes made to this config file...
-      notify  => Service['icinga2'],
-    }
-
+  Class['icinga2::config'] ->
+  file { "icinga2 object idomysqlconnection ${title}":
+    ensure  => file,
+    path    => $target_file,
+    owner   => $icinga2::params::config_owner,
+    group   => $icinga2::params::config_group,
+    mode    => $icinga2::params::config_mode,
+    content => template('icinga2/object/idomysqlconnection.conf.erb'),
   }
-  #...otherwise, use the same file resource but without a notify => parameter:
-  else {
 
-    file { "${target_dir}/${target_file_name}":
-      ensure  => $target_file_ensure,
-      owner   => $target_file_owner,
-      group   => $target_file_group,
-      mode    => $target_file_mode,
-      content => template('icinga2/object_idomysqlconnection.conf.erb'),
-    }
-
+  if $refresh_service == true {
+    File["icinga2 object idomysqlconnection ${title}"] ~> Class['icinga2::service']
   }
 
 }
