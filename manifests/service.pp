b7693ec3303d797b3ee59643a86dfe3d899f3ee3
Markus Frosch <markus@lazyfrosch.de>
2015-03-19 17:25:21 +0100

    Fix the service to use restart and clean the class a bit
    fixes #8820

--- manifests/service.pp
+++ manifests/service.pp
@@ -1,22 +1,15 @@
 # == Class: icinga2::service
 #
-# This class mangages the Icinga 2 daemon.
+# This class manages the Icinga 2 daemon.
 #
-# === Parameters
-#
-# Coming soon...
-#
-# === Examples
-#
-# Coming soon...
-#
-
 class icinga2::service inherits icinga2 {
 
-  #Service resource for the Icinga 2 daemon:
-  service {$icinga2::params::icinga2_daemon_name:
-    ensure    => running,
-    subscribe => [ Class['icinga2::config'], Class['icinga2::features'] ],
+  service { 'icinga2':
+    ensure     => running,
+    name       => $icinga2::params::icinga2_daemon_name,
+    enable     => true,
+    hasrestart => true,
+    subscribe  => [ Class['icinga2::config'], Class['icinga2::features'] ],
   }
 
 }
