404631c031dfaf1ee98a4b93ec56a85338462879
Markus Frosch <markus@lazyfrosch.de>
2015-06-11 11:58:04 +0200

    Add icinga2_config_value Puppet function and use it in the templates
    fixes #9107

---
+++ lib/puppet/parser/functions/icinga2_config_value.rb
@@ -0,0 +1,9 @@
+require "puppet/icinga2/utils"
+
+module Puppet::Parser::Functions
+  newfunction(:icinga2_config_value, :type => :rvalue) do |args|
+    raise Puppet::ParseError, 'Must provide exactly one argument to icinga2_config_value1' if args.length != 1
+
+    Puppet::Icinga2::Utils.config_value(args[0])
+  end
+end
