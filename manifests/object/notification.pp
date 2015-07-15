6a8111a9790fc36223cda00fa33665342f1559f9
Moritz Friedrich <mf@cyon.ch>
2015-07-15 14:59:53 +0200

    add template import option for notification

--- manifests/object/notification.pp
+++ manifests/object/notification.pp
@@ -12,6 +12,7 @@
 define icinga2::object::notification (
   $object_notificationname = $name,
   $is_template             = false,
+  $template_to_import      = undef,
   $command                 = undef,
   $host_name               = undef,
   $service_name            = undef,
