d49cb22ad88f5c4b89498ad72a1c93e8e98bf725
Reamer <philipp.dallig@gmail.com>
2015-11-23 18:27:13 +0100

    Add new graphite params

--- spec/defines/icinga2_object_graphitewriter_spec.rb
+++ spec/defines/icinga2_object_graphitewriter_spec.rb
@@ -49,7 +49,45 @@ describe 'icinga2::object::graphitewriter', :type => :define do
           }) }
       it { should contain_file(object_file).with_content(/^\s*host = "www.icinga.org"$/) }
       it { should contain_file(object_file).with_content(/^\s*port = 1234$/) }
+      it { should_not contain_file(object_file).with_content(/^\s*host_name_template/) }
+      it { should_not contain_file(object_file).with_content(/^\s*service_name_template/) }
+      it { should_not contain_file(object_file).with_content(/^\s*enable_send_thresholds/) }
+      it { should_not contain_file(object_file).with_content(/^\s*enable_send_metadata/) }
 
+    end
+    context "on #{name} with more non basic parameters" do
+      let :facts do
+        facts
+      end
+      let :params do
+      {
+        :host => 'www.icinga.org',
+        :port => '1234',
+        :host_name_template => 'icinga.$host.name$',
+        :service_name_template => 'icinga.$host.name$.$service.name$',
+        :enable_send_thresholds => true,
+        :enable_send_metadata => false,
+      }
+      end
+      let :pre_condition do
+        "include 'icinga2'"
+      end
+
+      let(:title) { 'graphite2testserver' }
+
+      object_file = '/etc/icinga2/features-available/graphite2testserver.conf'
+      it { should contain_icinga2__object__graphitewriter('graphite2testserver') }
+      it { should contain_file(object_file).with({
+            :ensure => 'file',
+            :path => '/etc/icinga2/features-available/graphite2testserver.conf',
+            :content => /object GraphiteWriter "graphite2testserver"/,
+          }) }
+      it { should contain_file(object_file).with_content(/^\s*host = "www.icinga.org"$/) }
+      it { should contain_file(object_file).with_content(/^\s*port = 1234$/) }
+      it { should contain_file(object_file).with_content(/^\s*host_name_template = icinga.\$host.name\$$/) }
+      it { should contain_file(object_file).with_content(/^\s*service_name_template = icinga.\$host.name\$.\$service.name\$$/) }
+      it { should contain_file(object_file).with_content(/^\s*enable_send_thresholds = true$/) }
+      it { should contain_file(object_file).with_content(/^\s*enable_send_metadata = false$/) }
     end
   end
 end
