c8fe24542d52f7108fbf27143eba37f56e1b3958
Reamer <philipp.dallig@gmail.com>
2015-12-31 11:33:21 +0100

    Correct link to Documentation

--- manifests/object/eventcommand.pp
+++ manifests/object/eventcommand.pp
@@ -2,7 +2,7 @@
 #
 # This is a defined type for Icinga 2 apply objects that create event commands
 # See the following Icinga 2 doc page for more info:
-# http://docs.icinga.org/icinga2/latest/doc/module/icinga2/chapter/configuring-icinga2#objecttype-eventcommand
+# http://docs.icinga.org/icinga2/latest/doc/module/icinga2/chapter/object-types#objecttype-eventcommand
 #
 # === Parameters
 #
--- manifests/object/zone.pp
+++ manifests/object/zone.pp
@@ -2,7 +2,7 @@
 #
 #  This is a defined type for Icinga 2 zone objects.
 # See the following Icinga 2 doc page for more info:
-# http://docs.icinga.org/icinga2/latest/doc/module/icinga2/chapter/configuring-icinga2#objecttype-zone
+# http://docs.icinga.org/icinga2/latest/doc/module/icinga2/chapter/object-types#objecttype-zone
 #
 # === Parameters
 #
