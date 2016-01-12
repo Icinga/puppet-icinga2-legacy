ceef2d95fa03b4016755a1cbfa2fc4723a00a876
Markus Frosch <markus@lazyfrosch.de>
2016-01-12 11:49:22 +0100

    Only expect hiera to auto-load data in Puppet 3 and newer
    refs #9148

--- spec/classes/icinga2__object_spec.rb
+++ spec/classes/icinga2__object_spec.rb
@@ -16,29 +16,32 @@ describe 'icinga2::object' do
     it { should contain_class('icinga2::object') }
     it { should contain_class('icinga2') }
 
-    it { should contain_icinga2__object__apiuser('admin') }
-    it { should contain_icinga2__object__apply_dependency('testdependency') }
-    it { should contain_icinga2__object__apply_notification_to_host('hostnotification') }
-    it { should contain_icinga2__object__apply_notification_to_service('servicenotification') }
-    it { should contain_icinga2__object__apply_service('oldservice') }
-    it { should contain_icinga2__object__apply_service('service') }
-    it { should contain_icinga2__object__checkcommand('test') }
-    it { should contain_icinga2__object__dependency('testdependency2') }
-    it { should contain_icinga2__object__eventcommand('test') }
-    it { should contain_icinga2__object__graphitewriter('test') }
-    it { should contain_icinga2__object__hostgroup('test') }
-    it { should contain_icinga2__object__host('testhierahost') }
-    it { should contain_icinga2__object__idomysqlconnection('test') }
-    it { should contain_icinga2__object__idopgsqlconnection('test') }
-    it { should contain_icinga2__object__notificationcommand('test') }
-    it { should contain_icinga2__object__notification('test') }
-    it { should contain_icinga2__object__perfdatawriter('test') }
-    it { should contain_icinga2__object__scheduleddowntime('test') }
-    it { should contain_icinga2__object__servicegroup('test') }
-    it { should contain_icinga2__object__service('test') }
-    it { should contain_icinga2__object__timeperiod('test') }
-    it { should contain_icinga2__object__usergroup('test') }
-    it { should contain_icinga2__object__user('test') }
-    it { should contain_icinga2__object__zone('test') }
+    # only expect hiera to auto-load data in Puppet 3 and newer
+    if Puppet.version >= '3.0'
+      it { should contain_icinga2__object__apiuser('admin') }
+      it { should contain_icinga2__object__apply_dependency('testdependency') }
+      it { should contain_icinga2__object__apply_notification_to_host('hostnotification') }
+      it { should contain_icinga2__object__apply_notification_to_service('servicenotification') }
+      it { should contain_icinga2__object__apply_service('oldservice') }
+      it { should contain_icinga2__object__apply_service('service') }
+      it { should contain_icinga2__object__checkcommand('test') }
+      it { should contain_icinga2__object__dependency('testdependency2') }
+      it { should contain_icinga2__object__eventcommand('test') }
+      it { should contain_icinga2__object__graphitewriter('test') }
+      it { should contain_icinga2__object__hostgroup('test') }
+      it { should contain_icinga2__object__host('testhierahost') }
+      it { should contain_icinga2__object__idomysqlconnection('test') }
+      it { should contain_icinga2__object__idopgsqlconnection('test') }
+      it { should contain_icinga2__object__notificationcommand('test') }
+      it { should contain_icinga2__object__notification('test') }
+      it { should contain_icinga2__object__perfdatawriter('test') }
+      it { should contain_icinga2__object__scheduleddowntime('test') }
+      it { should contain_icinga2__object__servicegroup('test') }
+      it { should contain_icinga2__object__service('test') }
+      it { should contain_icinga2__object__timeperiod('test') }
+      it { should contain_icinga2__object__usergroup('test') }
+      it { should contain_icinga2__object__user('test') }
+      it { should contain_icinga2__object__zone('test') }
+    end
   end
 end
