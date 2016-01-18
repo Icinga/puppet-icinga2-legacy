6cca2922592f4d39bad88e415fc28766e8e18d01
Jan <kroepke@outlook.com>
2016-01-18 23:59:23 +0100

    Fix some spec tests

--- spec/defines/icinga2_object_apply_service_spec.rb
+++ spec/defines/icinga2_object_apply_service_spec.rb
@@ -16,22 +16,22 @@ describe 'icinga2::object::apply_service', :type => :define do
       object_file = '/etc/icinga2/objects/applys/check_http.conf'
       it { should contain_icinga2__object__apply_service('check_http') }
       it { should contain_file(object_file).with({
-            :ensure => 'file',
-            :path => '/etc/icinga2/objects/applys/check_http.conf',
-            :content => /apply Service "check_http"/,
-          }) }
+                                                     :ensure => 'file',
+                                                     :path => '/etc/icinga2/objects/applys/check_http.conf',
+                                                     :content => /apply Service "check_http"/,
+                                                 }) }
       it { should contain_file(object_file).with_content(/^\s*import "generic-service"$/) }
       it { should_not contain_file(object_file).with_content(/^\s*check_command = "check_http"/) }
 
     end
-    context "on #{name} with custom_prepend parameter in string format" do
+    context "on #{name} with custom_prepend parameter" do
       let :facts do
         facts
       end
       let :params do
-      {
-        custom_prepend: => 'vars += config',
-      }
+        {
+            :custom_prepend =>  ['vars \+= config']
+        }
       end
       let :pre_condition do
         "include 'icinga2'"
@@ -42,26 +42,26 @@ describe 'icinga2::object::apply_service', :type => :define do
       object_file = '/etc/icinga2/objects/applys/check_http.conf'
       it { should contain_icinga2__object__apply_service('check_http') }
       it { should contain_file(object_file).with({
-            :ensure => 'file',
-            :path => '/etc/icinga2/objects/applys/check_http.conf',
-            :content => /apply Service "check_http"/,
-          }) }
+                                                     :ensure => 'file',
+                                                     :path => '/etc/icinga2/objects/applys/check_http.conf',
+                                                     :content => /apply Service "check_http"/,
+                                                 }) }
       it { should contain_file(object_file).with_content(/^\s*import "generic-service"$/) }
       it { should contain_file(object_file).with_content(/^\s*check_command = "check_http"/) }
-      it { should contain_file(object_file).with_content(/^\s*vars += config/) }
+      it { should contain_file(object_file).with_content(/^\s*vars \+= config/) }
     end
-    context "on #{name} with custom_append parameter in array format" do
+    context "on #{name} with custom_append parameter" do
       let :facts do
         facts
       end
       let :params do
-      {
-        custom_prepend: => ['if (host.vars.notification_type == "sms") {
+        {
+            :custom_prepend => ['if (host.vars.notification_type == "sms") {
          command = "sms-host-notification"
         } else {
          command = "mail-host-notification"
-        }', 'vars += config'],
-      }
+        }', 'vars \+= config']
+        }
       end
       let :pre_condition do
         "include 'icinga2'"
@@ -72,14 +72,29 @@ describe 'icinga2::object::apply_service', :type => :define do
       object_file = '/etc/icinga2/objects/applys/check_http.conf'
       it { should contain_icinga2__object__apply_service('check_http') }
       it { should contain_file(object_file).with({
-            :ensure => 'file',
-            :path => '/etc/icinga2/objects/applys/check_http.conf',
-            :content => /apply Service "check_http"/,
-          }) }
+                                                     :ensure => 'file',
+                                                     :path => '/etc/icinga2/objects/applys/check_http.conf',
+                                                     :content => /apply Service "check_http"/,
+                                                 }) }
       it { should contain_file(object_file).with_content(/^\s*import "generic-service"$/) }
-      it { should contain_file(object_file).with_content(/^\s*check_command = "check_http"/) }
-      it { should contain_file(object_file).with_content(/^\s*vars += config/) }
-      it { should contain_file(object_file).with_content(/^\s*if (host.vars.notification_type == "sms") \{\n\s+command = "sms-host-notification"\n\s+\} else \{\n\s+command = "mail-host-notification"\n\s+\}/) }
+      it { should contain_file(object_file).with_content(/^\s*check_command = "check_http"$/) }
+      it { should contain_file(object_file).with_content(/^\s*vars \+= config$/) }
+      it { should contain_file(object_file).with_content(/^\s*if \(host.vars.notification_type == "sms"\) \{\n\s+command = "sms-host-notification"\n\s+\} else \{\n\s+command = "mail-host-notification"\n\s+\}$/) }
+    end
+    context "on #{name} with invalid custom_append parameter" do
+      let :facts do
+        facts
+      end
+      let :params do
+        {
+            :custom_prepend =>  'invalid'
+        }
+      end
+      let :pre_condition do
+        "include 'icinga2'"
+      end
+
+      it { should raise_error(Puppet::Error, /is not an Array.\s+It looks to be a String/) }
     end
   end
 end
