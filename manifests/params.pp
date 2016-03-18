21158772cc8d378eee877a75f8471b4072dcf998
Moritz Friedrich <mf@cyon.ch>
2016-03-18 15:26:32 +0100

    add cloudlinux as supported operatingsystem

--- manifests/params.pp
+++ manifests/params.pp
@@ -44,7 +44,7 @@ class icinga2::params {
   ##################
   # Icinga 2 parameters by OS/release
   case $::operatingsystem {
-    'CentOS', 'RedHat', 'Scientific', 'OracleLinux', 'Amazon': {
+    'CentOS', 'RedHat', 'Scientific', 'OracleLinux', 'Amazon', 'CloudLinux': {
 
       #Packages for Nagios plugins:
       $plugin_packages = [
