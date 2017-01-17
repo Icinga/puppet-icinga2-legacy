9d1c787539dcb4d82306364116d1308cb514ba2f
Marco Fretz <marco.fretz@vshn.ch>
2017-01-17 14:51:52 +0100

    param to limit concurrent checks

--- manifests/feature/checker.pp
+++ manifests/feature/checker.pp
@@ -2,8 +2,14 @@
 #
 # Provides the check scheduler of Icinga 2.
 #
-class icinga2::feature::checker {
+class icinga2::feature::checker (
+    $concurrent_checks = undef,
+) {
 
-  ::icinga2::feature { 'checker': }
-
+  if $concurrent_checks {
+    validate_integer($concurrent_checks)
+  }
+  ::icinga2::feature { 'checker':
+    content => template('icinga2/feature/checker.conf.erb'),
+  }
 }
